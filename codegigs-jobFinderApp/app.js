04417e4f5948160e904eb128897439b972c1d492
DebarunMitra <debarun96mitra@gmail.com>
2020-10-15 15:47:54 +0530

    routes linked with db

--- codegigs-jobFinderApp/app.js
+++ codegigs-jobFinderApp/app.js
@@ -8,15 +8,24 @@ const app = express();
 //middleware
 app.use(express.json());
 
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+ 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Connect to db
 db.authenticate().then(()=>{
     console.log("Connection has been established successfully.");
   }).catch(error=>{
     console.error("Unable to connect to the database:", error);
-  });
- 
+  }); 
+  
+// Index route
+// app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
 
-app.get('/', (req,res)=>res.send("Running..."))
+// Gig routes
+app.use('/gigs', require('./routes/gigs'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server Up At ${PORT}`));
