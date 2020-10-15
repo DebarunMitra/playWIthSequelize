faf578d71ec9ac7d13defefc349df6fd904485bf
DebarunMitra <debarun96mitra@gmail.com>
2020-10-15 12:12:31 +0530

    db setup added

--- codegigs-jobFinderApp/app.js
+++ codegigs-jobFinderApp/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const db = require('./config/database');
 
 const app = express();
 
+//middleware
 app.use(express.json());
+
+//Connect to db
+db.authenticate().then(()=>{
+    console.log("Connection has been established successfully.");
+  }).catch(error=>{
+    console.error("Unable to connect to the database:", error);
+  });
+ 
 
 app.get('/', (req,res)=>res.send("Running..."))
 
---
+++ codegigs-jobFinderApp/config/database.js
@@ -0,0 +1,7 @@
+const { Sequelize } = require("sequelize");
+
+// Option 2: Passing parameters separately (other dialects)
+module.exports = new Sequelize("sequelize-db", "postgres", "root", {
+  host: "localhost",
+  dialect: "postgres" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+});
